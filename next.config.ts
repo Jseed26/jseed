4c3ca436aba4cd490ab89c74f90a4ade27a598fb
Hadar Liel Harush <hadarharush5@gmail.com>
2026-08-17 17:53:25 +0300

    remove pwa completely to fix poisoned sw

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,4 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  
-  // 👇 כיבינו את הקאשינג האגרסיבי שגרם לנעילה
-  cacheOnFrontEndNav: false,
-  aggressiveFrontEndNavCaching: false,
-  
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  
-  // 👇 פקודת ההשמדה לגרסאות הישנות שתציל את שאר הטלפונים
-  workboxOptions: {
-    skipWaiting: true, // מכריח את הטלפון לעבור מיד לגרסה החדשה
-    clientsClaim: true, // גורם לגרסה החדשה להשתלט מיד על כל הדפים הפתוחים
-  }
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
